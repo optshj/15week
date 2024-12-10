2d30a4fb1095aac8670791b4167bf7edae69077d
optshj <optshj@gmail.com>
2024-12-10 20:35:05 +0900

    수정

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+
 app.get("/", (req, res) => {
-res.send("<h1> Incheon National University !!</h1>");
+  res.send("<h1>Incheon National University !!</h1>");
 });
-app.listen(port, () => {
-console.log(`Example app listening at http://localhost:${port}`);
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Example app listening at http://0.0.0.0:${port}`);
 });
